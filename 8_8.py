141aa3990acad07901309a77a8eccb83d7c560ff
Nicolas <nicolasmilanijr@gmail.com>
2025-05-26 12:19:17 -0300

    Add files via upload

---
+++ 8_8.py
@@ -0,0 +1,7 @@
+#primeiro faça as variaveis
+#depois faça as contas
+# depois o resultado
+m=float(input("qual e o seu salario"))
+n= (m*0.15)
+aumento=n+m
+print (f"o seu salario era de {m} depois do almento de 15% ficou de {aumento } ")
